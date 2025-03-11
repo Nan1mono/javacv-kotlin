e6154bea7e832e1e91740e9e026933aca0681856
nanimono <nan1mono@163.com>
2025-03-11 15:10:08 +0800

    refactor(server): 重构服务器端代码并更新配置
    - 修改 pom.xml 中的主类配置，合并为单一入口点
    - 优化 Server.kt 中的端口配置，使用 YAML 文件读取端口信息
    - 删除了不必要的类和代码，简化了项目结构

--- src/main/kotlin/com/project/video/server/Server.kt
+++ src/main/kotlin/com/project/video/server/Server.kt
@@ -1,12 +1,19 @@
 package com.project.video.server
 
 import com.project.video.server.handler.VideoSocketServer
+import com.project.video.toolkit.ReadYamlUtils
 
 class Server {
+
+
+
     companion object{
+        private val serverConfig = ReadYamlUtils.readConfig()["server"] as Map<*, *>
+        private val serverPort = serverConfig["port"] as Int
+
         @JvmStatic
         fun main(args: Array<String>) {
-            val videoSocketServer = VideoSocketServer(8333)
+            val videoSocketServer = VideoSocketServer(serverPort)
             videoSocketServer.start()
         }
     }
